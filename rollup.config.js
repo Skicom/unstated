140f2f262319b3112a80dffd8db76cede38c1767
Enrique <enrique@ski.com>
2018-12-06 12:39:15 -0700

    moved dependencies around

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+import noderesolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/unstated.js',
@@ -14,8 +16,16 @@ export default {
     }
   ],
   external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
+    ...Object.keys(pkg.peerDependencies || {}),
+    ...Object.keys(pkg.devDependencies || {})
   ],
-  plugins: [babel()]
+  plugins: [
+    noderesolve({ browser: true, extensions: ['.js', '.jsx', '.json'] }),
+    commonjs({
+      include: ['node_modules/**']
+    }),
+    babel({
+      babelrc: true
+    })
+  ]
 };
